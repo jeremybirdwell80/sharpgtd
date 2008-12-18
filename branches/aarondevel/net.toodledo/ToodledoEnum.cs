a227ae4c1c59376f3002122741065163bfa50fe5
aaronfriedel <aaronfriedel@14315a16-714d-0410-8da2-cdca801a996a>
2008-12-18 04:00:06 +0000

    Added enums for Priority and Repeat.  This is just preperation and aren't used anywhere yet.

---
+++ branches/aarondevel/net.toodledo/ToodledoEnum.cs
@@ -0,0 +1,23 @@
+﻿public enum Priority
+{
+    Invalid = -100,
+    Negative = -1,
+    Low =0,
+    Medium=1,
+    High=2,
+    Top=3
+}
+
+public enum Repeat
+{
+    NoRepeat =0,
+    Weekly =1,
+    Monthly =2,
+    Yearly =3,
+    Daily=4,
+    Biweekly=5,
+    Bimonthly=6,
+    Semiannually=7,
+    Quarterly=8,
+    WithParent=9
+}
